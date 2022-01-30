8b5697efed6f144d63e4323f16ffabaed23f8d8c
Highlander Robotics <frc8033@piedmontmakers.org>
2022-01-29 16:13:43 -0800

    Added motor testing capability for shooter testing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,11 +4,23 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.TestingSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.util.sendable.Sendable;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -22,10 +34,22 @@ public class RobotContainer {
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
+  private final TestingSubsystem m_testingSubsystem = new TestingSubsystem();
+  private ShuffleboardTab tab = Shuffleboard.getTab("Testing");
+
+
+  private double rpm = 2300;
+
+
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
+    SmartDashboard.putNumber("Encoder", m_testingSubsystem.testingMotor.getSelectedSensorVelocity() * (1.0/2048.0) * 600.0);
+    SmartDashboard.putData("Toggle Motor", new RunCommand(() -> m_testingSubsystem.setTestingRPM(rpm), m_testingSubsystem));
+    // SmartDashboard.putNumber("Motor Set RPM", 0);
+    
+    
   }
 
   /**
@@ -34,7 +58,10 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+
+
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
---
+++ src/main/java/frc/robot/subsystems/TestingSubsystem.java
@@ -0,0 +1,68 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class TestingSubsystem extends SubsystemBase {
+
+  public final WPI_TalonFX testingMotor;
+
+
+  public double rpmTarget = 200;
+
+
+  private ShuffleboardTab tab = Shuffleboard.getTab("Testing");
+  private NetworkTableEntry rpmSetpoint =
+    tab.add("Set Motor RPM", 0)
+    .getEntry();
+  private NetworkTableEntry currentSetpointEntry =
+    tab.add("789Current RPM Setpoint", rpmTarget)
+    .getEntry();
+  private NetworkTableEntry currentRPM =
+    tab.add("Motor Current RPM", getRPM())
+    .getEntry();
+  private NetworkTableEntry currentMotorControllerSetpoint =
+    tab.add("Current Motor controller setpoint", 0)
+    .getEntry();
+  private NetworkTableEntry encoder =
+    tab.add("encoder", 0)
+    .getEntry();
+
+  /** Creates a new TestingSubsystem. */
+  public TestingSubsystem() {
+    testingMotor = new WPI_TalonFX(0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    rpmTarget = rpmSetpoint.getDouble(0);
+    currentSetpointEntry.setDouble(rpmTarget);
+    currentRPM.setDouble(getRPM());
+    encoder.setDouble(testingMotor.getSelectedSensorVelocity() * (1.0/2048.0) * 600.0);
+  }
+
+  public void setTestingRPM(double rpm){
+    double targetVelocity = (rpm * 2048) / 600;
+    testingMotor.set(TalonFXControlMode.Velocity, targetVelocity);
+  }
+
+  public double getRPM(){
+    if (testingMotor != null){
+       return testingMotor.getSelectedSensorVelocity()* (1.0/2048.0) * 600.0;
+    } else {
+      return 0;
+    }
+  }
+}
