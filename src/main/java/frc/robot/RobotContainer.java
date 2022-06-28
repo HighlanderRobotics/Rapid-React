1ecd9833bbcdd94e9baf1793690cd8eaf98cabc1
Highlander Robotics <frc8033@piedmontmakers.org>
2022-06-28 15:30:36 -0700

    disabled climbersubsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -81,7 +81,7 @@ public class RobotContainer {
   private final VisionSubsystem visionSubsystem = new VisionSubsystem(new LimeLightSubsystem("limelight-top"), limeLightSubsystem);
   private final RoutingSubsystem routingSubsystem = new RoutingSubsystem();
   private final LEDSubsystem ledSubsystem = new LEDSubsystem();
-  private final ClimberSubsystem climberSubsystem = new ClimberSubsystem();
+  // private final ClimberSubsystem climberSubsystem = new ClimberSubsystem();
 
   private final SlewRateLimiter forwardLimiter = new SlewRateLimiter(3.5);
   private final SlewRateLimiter strafeLimiter = new SlewRateLimiter(3.5);
@@ -139,8 +139,8 @@ public class RobotContainer {
     // SmartDashboard.putData("Reset Hood", new ResetHood(hoodSubsystem));
     // SmartDashboard.putData("Shoot one ball", new ShootOneBall(routingSubsystem));
     // SmartDashboard.putData("Route one ball", new RouteOneBall(routingSubsystem));
-    SmartDashboard.putData("lock ratchet", new InstantCommand(() -> climberSubsystem.lockRatchet()));
-    SmartDashboard.putData("unlock ratchet", new InstantCommand(() -> climberSubsystem.unlockRatchet()));
+    // SmartDashboard.putData("lock ratchet", new InstantCommand(() -> climberSubsystem.lockRatchet()));
+    // SmartDashboard.putData("unlock ratchet", new InstantCommand(() -> climberSubsystem.unlockRatchet()));
     // SmartDashboard.putData("Run Routing for Shooting", new RunCommand(() -> {routingSubsystem.setOuterFeederRPM(700); routingSubsystem.setInnerFeederRPM(500);}, routingSubsystem));
     // SmartDashboard.putData("Shoot two balls", new ShootTwoBalls(routingSubsystem, shooterSubsystem));
     // SmartDashboard.putData("Extend Intake", new RunCommand(() -> intakeSubsystem.extend(), intakeSubsystem));
@@ -153,7 +153,7 @@ public class RobotContainer {
     // SmartDashboard.putData("Toggle Intake", new InstantCommand(() -> m_intakeSubsystem.toggleIntake(), m_intakeSubsystem));
     // SmartDashboard.putData("Auto Aim", new AutoAim(m_visionSubsystem, m_drivetrainSubsystem));
     
-    climberSubsystem.setDefaultCommand(new RunCommand(() -> climberSubsystem.retractIfLocked(controller.getRightTriggerAxis() * -0.6), climberSubsystem));
+    // climberSubsystem.setDefaultCommand(new RunCommand(() -> climberSubsystem.retractIfLocked(controller.getRightTriggerAxis() * -0.6), climberSubsystem));
     intakeSubsystem.setDefaultCommand(new RunCommand(() -> {intakeSubsystem.retract(); intakeSubsystem.setIntakeRPM(0);}, intakeSubsystem));
     shooterSubsystem.setDefaultCommand(new RunCommand(() -> shooterSubsystem.setTargetRPM(0), shooterSubsystem));
     hoodSubsystem.setDefaultCommand(new RunCommand(() -> hoodSubsystem.setSetpoint(hoodTarget), hoodSubsystem));
@@ -196,16 +196,16 @@ public class RobotContainer {
 
   
 
-    new Button(operator::getAButton)
-      .toggleWhenPressed(new ExtendClimber(climberSubsystem, ledSubsystem, 38, 20.0));
-    new Button(operator::getBButton)
-      .whenActive(new RetractClimber(climberSubsystem));
-    new Button(operator::getLeftBumper)
-      .whenPressed(new InstantCommand(() -> climberSubsystem.decreaseAngle(0.5)));
-    new Button(operator::getRightBumper)
-      .toggleWhenPressed(new IncreaseExtension(climberSubsystem));
-    new Button(operator::getStartButton)
-      .whenPressed(new InstantCommand(() -> climberSubsystem.extendedAndLocked = false));
+    // new Button(operator::getAButton)
+    //   .toggleWhenPressed(new ExtendClimber(climberSubsystem, ledSubsystem, 38, 20.0));
+    // new Button(operator::getBButton)
+    //   .whenActive(new RetractClimber(climberSubsystem));
+    // new Button(operator::getLeftBumper)
+    //   .whenPressed(new InstantCommand(() -> climberSubsystem.decreaseAngle(0.5)));
+    // new Button(operator::getRightBumper)
+    //   .toggleWhenPressed(new IncreaseExtension(climberSubsystem));
+    // new Button(operator::getStartButton)
+    //   .whenPressed(new InstantCommand(() -> climberSubsystem.extendedAndLocked = false));
 
   }
 
