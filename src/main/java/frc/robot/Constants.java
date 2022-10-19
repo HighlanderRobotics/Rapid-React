2614196057855f3d2b4cafe17a8f8e00d1741c44
Lewis-Seiden <aPotatoeWithANaginata@gmail.com>
2022-10-19 09:31:44 -0700

    Comments added to constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -22,65 +22,73 @@ public final class Constants {
      *
      * Should be measured from center to center.
      */
-    public static final double DRIVETRAIN_TRACKWIDTH_METERS = 0.44; // FIXME Measure and set trackwidth
+    public static final double DRIVETRAIN_TRACKWIDTH_METERS = 0.44; 
     /**
      * The front-to-back distance between the drivetrain wheels.
      *
      * Should be measured from center to center.
      */
-    public static final double DRIVETRAIN_WHEELBASE_METERS = 0.44; // FIXME Measure and set wheelbase
+    public static final double DRIVETRAIN_WHEELBASE_METERS = 0.44; 
 
     public static final double ROTATION_SPEED_MULTPILIER = 0.55;
-    public static final int DRIVETRAIN_PIGEON_ID = 0; // FIXME Set Pigeon ID
+    public static final int DRIVETRAIN_PIGEON_ID = 0; 
 
-    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 6; // FIXME Set front left module drive motor ID
-    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 5; // FIXME Set front left module steer motor ID
-    public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 21; // FIXME Set front left steer encoder ID
+    // Swerve motor CAN ids and encoder offsets
+
+    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 6; 
+    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 5;
+    public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 21;
     public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(134 + 180); // was 134 + 180
 
-    public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 2; // FIXME Set front right drive motor ID
-    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 1; // FIXME Set front right steer motor ID
-    public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 22; // FIXME Set front right steer encoder ID
+    public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 2;
+    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 1;
+    public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 22; 
     public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0); // was 0
 
-    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 4; // FIXME Set back left drive motor ID
-    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 3; // FIXME Set back left steer motor ID
-    public static final int BACK_LEFT_MODULE_STEER_ENCODER = 20; // FIXME Set back left steer encoder ID
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(129 + 180); // FIXME Measure and set back left steer offset
+    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 4; 
+    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 3; 
+    public static final int BACK_LEFT_MODULE_STEER_ENCODER = 20; 
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(129 + 180); 
 
-    
-    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 8; // FIXME Set back right drive motor ID
-    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 7; // FIXME Set back right steer motor ID
-    public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 23; // FIXME Set back right steer encoder ID
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(107 + 180); // FIXME Measure and set back right steer offset
+    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 8;
+    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 7; 
+    public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 23; 
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(107 + 180); 
 
+    // Routing modor CAN ids
     public static final int INNER_FEEDER_MOTOR = 9;
     public static final int OUTER_FEEDER_MOTOR = 12;
-    public static final int FLYWHEEL_MOTOR = 10;
-    public static final int HOOD_ANGLE_MOTOR = 11;
-
-    public static final int HOOD_ENCODER_A = 7; // dio ports for A and B for quadrature on rev encoder for hood
-    public static final int HOOD_ENCODER_B = 8;
-
-    public static final int HOOD_LIMIT_SWITCH_TOP = 3;
-    public static final int HOOD_LIMIT_SWITCH_BOTTOM = 2;
-    
-    public static final int INTAKE_MOTOR = 18;
-    public static final int INTAKE_SOLENOID_FORWARD = 0;
-    public static final int INTAKE_SOLENOID_BACKWARD = 1;
+    // Routing beambreak ports
     public static final int LOWER_BEAMBREAK = 4;
     public static final int UPPER_BEAMBREAK = 5;
 
+    // Flywheel and hood CAN ids
+    public static final int FLYWHEEL_MOTOR = 10;
+    public static final int HOOD_ANGLE_MOTOR = 11;
+
+    // DIO ports for A and B for quadrature on rev encoder for hood
+    public static final int HOOD_ENCODER_A = 7; 
+    public static final int HOOD_ENCODER_B = 8;
+
+    // DIO ports for the hood limit switches
+    public static final int HOOD_LIMIT_SWITCH_TOP = 3;
+    public static final int HOOD_LIMIT_SWITCH_BOTTOM = 2;
+    
+    // Intake motor CAN id
+    public static final int INTAKE_MOTOR = 18;
+    // Intake solenoid port
+    public static final int INTAKE_SOLENOID_FORWARD = 0;
+    public static final int INTAKE_SOLENOID_BACKWARD = 1;
+    
+    // Climber motor CAN ids. Angle motor is not used on the new climber
     public static final int CLIMBER_ANGLE_MOTOR = 16;
     public static final int CLIMBER_EXTENSION_MOTOR = 15;
     public static final int CLIMBER_LIMIT_SWITCH = 1;
     public static final int CLIMBER_RATCHET_SERVO = 9;
+    // Solenoids for releasing the passive arms on the new climber
     public static final int CLIMBER_SOLENOID_FORWARD = 2;
     public static final int CLIMBER_SOLENOID_BACKWARD = 3;
-
-    public static final double ksVolts = 4.15;
-    // public static final double ksVoltS
-    //econdsPerMeter = 0.0006;
-    // public static final double kaVoltSecondsSquaredPerMeter = -0.0055;
+    
+    // Port for the all-important LEDS
     public static final int LED_PORT = 8;
 }
