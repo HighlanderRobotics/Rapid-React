99e491f6b5c41bd6b3a4673c5012df05fffd2766
Lewis-Seiden <aPotatoeWithANaginata@gmail.com>
2022-10-19 10:33:08 -0700

    Added comments to limelight subsystem

--- src/main/java/frc/robot/subsystems/LimeLightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimeLightSubsystem.java
@@ -12,8 +12,6 @@ import frc.robot.Constants;
 import io.github.oblarg.oblog.Loggable;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
@@ -21,16 +19,21 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 
 /** Contains a single limelight. Could probably take over VisionSubsystem. */
 public class LimeLightSubsystem extends SubsystemBase implements Loggable{
+  // Current horizontal and vertical angle to target
   public double horizontalOffset; 
   public double verticalOffset;
+  // Current area of target in vision
   public double areaOffset;
-  NetworkTable table;
+  // Redundant with horizontal and vertical offset
   public double x;
   public double y;
+  // Output of pid to snap to target (left to right)
   public double pidOutput = 0;
+  // Whether a target is in view
   public boolean isPointingAtTarget;
+  // PID controller to snap to target (left to right)
   public PIDController limelightPID = Constants.AUTOAIM_PID_CONTROLLER;
-  private ShuffleboardTab tab = Shuffleboard.getTab("Drive Readouts");
+  // Limelight network table
   public String tableName;
 
   
@@ -39,36 +42,29 @@ public class LimeLightSubsystem extends SubsystemBase implements Loggable{
     lightOn();
   }
 
- 
+ /**Reads the network tables limelight values to x and y */
   public void defaultReadings() {
-    
-
     //read values periodically
     x = NetworkTableInstance.getDefault().getTable(tableName).getEntry("tx").getDouble(0.0);
     y = NetworkTableInstance.getDefault().getTable(tableName).getEntry("ty").getDouble(0.0);
-    double area = NetworkTableInstance.getDefault().getTable(tableName).getEntry("ta").getDouble(0.0);
-
-
-    
   }
 
-  public void lightReadings() {
-    
-  }
-
+  /**Returns the current area of the target */
   public double getArea() {
     return areaOffset;
   }
 
+  /**Returns the current horizontal angle to the target */
   public double getHorizontalOffset() {
     return horizontalOffset;
   }
 
+  /**Returns the current vertical angle to the target */
   public double getVerticalOffset() {
     return verticalOffset;
   }
 
-  // distance to target in feet
+  /**Calculates and returns the current distance to the target in feet */
   //@Log
   public double getDistance() {
     // angle is in degrees and Vaughn says it's mounted at 52 degrees
@@ -79,23 +75,28 @@ public class LimeLightSubsystem extends SubsystemBase implements Loggable{
     return distance / 12.0;
   }
 
+  /**Turns on the limelight leds */
   public void lightOn() {
     NetworkTableInstance.getDefault().getTable(tableName).getEntry("ledMode").setNumber(0);
   }
 
+  /**Turns off the limelight leds */
   public void lightOff() {
     NetworkTableInstance.getDefault().getTable(tableName).getEntry("ledMode").setNumber(1);
   }
 
+  /**Returns whether a target is visible */
   public boolean isPointingAtTarget() {
     return isPointingAtTarget;
   }
 
+  /**Calculates and returns the pid output */
   public double autoAim () {
     double output = limelightPID.calculate(horizontalOffset);
     return output;
   }
 
+  /**Sets a controller to rumble based on how far to the left or right the target is */
   public void controllerRumble(XboxController controller) {
     controller.setRumble(RumbleType.kLeftRumble, -horizontalOffset * 0.025);
     controller.setRumble(RumbleType.kRightRumble, horizontalOffset * 0.025);
@@ -106,7 +107,6 @@ public class LimeLightSubsystem extends SubsystemBase implements Loggable{
     // This method will be called once per scheduler run
     horizontalOffset = NetworkTableInstance.getDefault().getTable(tableName).getEntry("tx").getDouble(0.0);
     verticalOffset = NetworkTableInstance.getDefault().getTable(tableName).getEntry("ty").getDouble(0.0);
-    double area = NetworkTableInstance.getDefault().getTable(tableName).getEntry("ta").getDouble(0.0);
     isPointingAtTarget = NetworkTableInstance.getDefault().getTable(tableName).getEntry("tv").getDouble(0.0) == 1;
     //SmartDashboard.putNumber("limelightX", horizontalOffset);
     //SmartDashboard.putNumber("limelightY", verticalOffset);
